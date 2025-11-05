314b42d1fcd3696c77a7bb580b4b066059545df9
ashok792322 <ashok.mailmail@gmail.com>
2025-11-05 14:22:31 +0530

    Assignment2_Task1 and Assignment2_Task2
    Module 3 Assignment Task 1 and Tas2 completed

---
+++ Assignment2_task1.py
@@ -0,0 +1,10 @@
+# Assignment2_Task1
+# Task1:Check if a Number is Even or Odd.
+num = int(input("Enter a number: "))     # num is assigned value entered by user through input()
+if num % 2 == 0:                         # if statement checking condition for even or odd, Give True or False as per user input received from line 1
+    print(f"{num} is a even number")     # If block start , control enter if condition is true.
+else:                                    # else statement for if conditional statement false
+    print(f"{num} is a odd number")      # else block for if conditional statement False
+
+
+    
