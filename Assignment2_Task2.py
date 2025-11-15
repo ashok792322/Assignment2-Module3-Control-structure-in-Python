15fcde3b35d3e4adf3675392bd0a464ae741e0a2
ashok792322 <ashok.mailmail@gmail.com>
2025-11-15 11:47:45 +0530

    Update Assignment2_Task2.py
    Rectification done in Assignment2_Task2.  line 4 rage updated from (1,50) to (1,51) as 51 will excluded and for loop iterate from 1 to 50

--- Assignment2_Task2.py
+++ Assignment2_Task2.py
@@ -1,6 +1,7 @@
 #Assignment2_Task2
 #Task2: Sum of integer from 1 to 50 using loop
 sum=0                                               # variable declared with initial value 0
-for i in range(1,50):                               # for loop iteration from range 1 to 50
+for i in range(1,51):                               # for loop iteration from range 1 to 51, 51 excluded so that for loop iterate from 1 to 50
     sum += i                                        # sum=sum+1 can be written with urinary operator, with each iteration incremental value of i added to variable sum and assign to sum itself.
 print(f"The sum of number from 1 to 50 is: {sum}")  # last added value store in variable sum inside for_block is call outside for_block to print.
+
